b4a2f3a0e428a0fafb0a08bfbf49b004aff551d1
Ujwal budha <ujjwal.budha07@gmail.com>
2024-06-06 22:04:05 +0545

    added userdata to install Docker and Docker-Compose

--- terraform/script/automate.sh
+++ terraform/script/automate.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install git -y
-sudo amazon-linux-extras install epel -y
-sudo yum install ansible -y
-cd ~
-aws s3 sync s3://amazon-linux-ansible-code-ujwal/ ~/ansible/
-cd ~/ansible
-ansible-playbook --connection=local --inventory 127.0.0.1, playbook.yml
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+sudo apt-get update -y
+
+# Install required packages for Docker installation
+echo "Installing required packages..."
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+# Add Docker's official GPG key
+echo "Adding Docker's official GPG key..."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add Docker's official APT repository
+echo "Adding Docker's official APT repository..."
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Start Docker service and enable it
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+
+# Install Docker Compose
+echo "Installing Docker Compose..."
+DOCKER_COMPOSE_VERSION="1.29.2"
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+echo "Applying executable permissions to the Docker Compose binary..."
+sudo chmod +x /usr/local/bin/docker-compose
