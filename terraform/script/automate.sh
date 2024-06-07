7cb82e459c72b4c0eb440e319da002c91a8bb50f
Ujwal budha <ujjwal.budha07@gmail.com>
2024-06-07 07:25:27 +0545

    added ansible to configure server

--- terraform/script/automate.sh
+++ terraform/script/automate.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-sudo yum update -y
+sudo apt update -y
 #!/bin/bash
 
 # Exit immediately if a command exits with a non-zero status
 set -e
 sudo apt-get update -y
-sudo snap install aws-cli --classic -y 
+sudo snap install aws-cli --classic
+
+# Update the system
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Install Git
+sudo apt-get install git -y
+
+# Add Ansible PPA and install Ansible
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt-get install ansible -y
+
+# Install AWS CLI if not already installed
+sudo apt-get install awscli -y
+
 
 # Install required packages for Docker installation
 echo "Installing required packages..."
@@ -41,20 +57,29 @@ sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPO
 echo "Applying executable permissions to the Docker Compose binary..."
 sudo chmod +x /usr/local/bin/docker-compose
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 426857564226.dkr.ecr.us-east-1.amazonaws.com
-docker pull 426857564226.dkr.ecr.us-east-1.amazonaws.com/testujwal001:frontend
-docker pull 426857564226.dkr.ecr.us-east-1.amazonaws.com/testujwal001:backend
+# Sync files from the S3 bucket to the local machine
+aws s3 sync s3://docker_test_bucket_intern/ ~/ansible/
 
-cd ~
-echo "version: '3'
-services:
-  frontend:
-    image: 426857564226.dkr.ecr.us-east-1.amazonaws.com/testujwal001:frontend
-    ports:
-      - "8080:80"
-  backend:
-    image: 426857564226.dkr.ecr.us-east-1.amazonaws.com/testujwal001:backend
-    ports:
-      - "8081:80"
-" > docker-compose.yml
-docker-compose up --build -d
+# Change directory to the ansible directory
+cd ~/ansible
+
+# Run the Ansible playbook locally
+ansible-playbook --connection=local --inventory localhost, playbook.yml
+
+# aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 426857564226.dkr.ecr.us-east-1.amazonaws.com
+# docker pull 426857564226.dkr.ecr.us-east-1.amazonaws.com/testujwal001:frontend
+# docker pull 426857564226.dkr.ecr.us-east-1.amazonaws.com/testujwal001:backend
+
+# cd ~
+# echo "version: '3'
+# services:
+#   frontend:
+#     image: 426857564226.dkr.ecr.us-east-1.amazonaws.com/testujwal001:frontend
+#     ports:
+#       - "8080:80"
+#   backend:
+#     image: 426857564226.dkr.ecr.us-east-1.amazonaws.com/testujwal001:backend
+#     ports:
+#       - "8081:80"
+# " > docker-compose.yml
+# docker-compose up --build -d
