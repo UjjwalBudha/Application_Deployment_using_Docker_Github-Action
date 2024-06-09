dc6dfcee2cb0bb1e151d25baf091bab53668a0d9
Ujwal budha <ujjwal.budha07@gmail.com>
2024-06-09 09:52:13 +0545

    used env variable for backend.tf bucket key

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,7 @@
-# terraform {
-#   backend "s3" {}
-# }
 terraform {
   backend "s3" {
     region = "us-east-1"
-    key    = "426857564226/ujwal-ansible-amazon-linux-2.tfstate"
+    # key    = "426857564226/ujwal-docker-pipline.tfstate"
     bucket = "8586-terraform-state"
   }
 }
